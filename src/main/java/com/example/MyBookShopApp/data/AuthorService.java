c671fb55c3a740130bf97aeba4681907dc87e69f
Boris <shramko.boris@gmail.com>
2022-01-30 17:03:31 +0700

    Work1 (sorted authors) v.2

--- src/main/java/com/example/MyBookShopApp/data/AuthorService.java
+++ src/main/java/com/example/MyBookShopApp/data/AuthorService.java
@@ -6,8 +6,7 @@ import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Service;
 
 import java.sql.ResultSet;
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
 
 @Service
 public class AuthorService {
@@ -19,12 +18,24 @@ public class AuthorService {
         this.jdbcTemplate = jdbcTemplate;
     }
 
-    public List<Author> getAuthorsData() {
+    public Map<Character, ArrayList<Author>> getAuthorsData() {
         List<Author> authors = jdbcTemplate.query("SELECT author FROM books", (ResultSet rs, int rowNum) -> {
             Author author = new Author();
             author.setName(rs.getString("author"));
             return author;
         });
-        return new ArrayList<>(authors);
+        authors.sort((o1, o2) -> o1.toString().compareTo(o2.toString()));
+        Map<Character, ArrayList<Author>> authorsMap = new HashMap<>();
+        for (Author author : authors) {
+            Character letter = author.getName().charAt(0);
+            if (authorsMap.get(letter) == null) {
+                authorsMap.put(letter, new ArrayList<>());
+                authorsMap.get(letter).add(author);
+            } else {
+                authorsMap.get(letter).add(author);
+                authorsMap.put(letter, authorsMap.get(letter));
+            }
+        }
+        return authorsMap;
     }
 }
